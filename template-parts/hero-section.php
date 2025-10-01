5de4d618af6f5fa3df89924cba5951db218cd624
shahidasghar90 <shahidasghar90@yahoo.com>
2025-10-01 18:49:35 +0200

    fix hero section

--- template-parts/hero-section.php
+++ template-parts/hero-section.php
@@ -7,26 +7,26 @@
             <div class="hero-content wow fadeInUp mx-auto max-w-[780px] text-center" data-wow-delay=".2s">
               <h1 class="mb-6 text-3xl font-bold leading-snug text-white sm:text-4xl sm:leading-snug lg:text-5xl lg:leading-[1.2]">
                 <!-- Open-Source Web Template for SaaS, Startup, Apps, and More -->
-                <?php echo get_theme_mod('hero_title', 'Open-Source Web Template for SaaS, Startup, Apps, and More'); ?>
+                <?php echo get_theme_mod('hero_title', 'Bringing Your Digital Vision to Life.'); ?>
               </h1>
               <p class="mx-auto mb-9 max-w-[600px] text-base font-medium text-white sm:text-lg sm:leading-[1.44]">
-                <?php echo get_theme_mod('hero_subtitle', 'Multidisciplinary Web Template Built with Your Favourite Technology - HTML Bootstrap, Tailwind and React NextJS.'); ?>
+                <?php echo get_theme_mod('hero_subtitle', 'We specialize in crafting seamless and dynamic web experiences, blending the latest in front-end and back-end technologies to deliver exceptional digital solutions.'); ?>
               </p>
               <ul class="flex flex-wrap items-center justify-center gap-5 mb-10">
                 <li>
-                  <a href="https://links.tailgrids.com/play-download" class="inline-flex items-center justify-center rounded-md bg-white px-7 py-[14px] text-center text-base font-medium text-dark shadow-1 transition duration-300 ease-in-out hover:bg-gray-2 hover:text-body-color">
-                     <?php echo get_theme_mod('hero_button_one_text', 'Download Now'); ?>
+                  <a href="https://ims.devdinos.com/" class="inline-flex items-center justify-center rounded-md bg-white px-7 py-[14px] text-center text-base font-medium text-dark shadow-1 transition duration-300 ease-in-out hover:bg-gray-2 hover:text-body-color">
+                     <?php echo get_theme_mod('hero_button_one_text', 'IMS Login'); ?>
                   </a>
                 </li>
                 <li>
-                  <a href="https://github.com/tailgrids/play-tailwind" target="_blank" class="flex items-center gap-4 rounded-md bg-white/[0.12] px-6 py-[14px] text-base font-medium text-white transition duration-300 ease-in-out hover:bg-white hover:text-dark">
-                     <?php echo get_theme_mod('hero_button_two_text', 'Star on Github'); ?>
+                  <a href="https://dino-wealth.vercel.app/" target="_blank" class="flex items-center gap-4 rounded-md bg-white/[0.12] px-6 py-[14px] text-base font-medium text-white transition duration-300 ease-in-out hover:bg-white hover:text-dark">
+                     <?php echo get_theme_mod('hero_button_two_text', 'Dino-Wealth Login'); ?>
                   </a>
                 </li>
               </ul>
               <div>
                  <p class="mb-4 text-base font-medium text-center text-white">
-                  <?php echo get_theme_mod('brands_title', 'Built with latest technology'); ?>
+                  <?php echo get_theme_mod('brands_title', 'Driven by next-gen technology'); ?>
                 </p>
                 <div class="flex items-center justify-center gap-4 text-center wow fadeInUp" data-wow-delay=".3s">
                   <?php
@@ -43,7 +43,7 @@
                     if ($image) :
                   ?>
                       <a href="<?php echo esc_url($url); ?>" class="duration-300 ease-in-out text-white/60 hover:text-white">
-                        <img src="<?php echo esc_url($image); ?>" alt="brand-<?php echo $i; ?>" />
+                        <img src="<?php echo esc_url($image); ?>" alt="brand-<?php echo $i; ?>" class="h-auto w-16" />
                       </a>
                   <?php
                     endif;
